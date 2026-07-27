852f21a2c90e39e64a393e4d67c68fd0880a9bf8
prajwal <prajwalprajwal8385@gmail.com>
2026-07-27 21:41:12 +0530

    hlo

--- src/services/mondayService.ts
+++ src/services/mondayService.ts
@@ -31,8 +31,11 @@ export const fetchMondayBoard = async (
 ): Promise<MondayBoardData> => {
   try {
     const API_URL = import.meta.env.VITE_API_URL;
-
+   console.log("API_URL =", API_URL);
+   console.log("ENV =", import.meta.env);
 const response = await fetch(`${API_URL}/monday/${boardId}`);
+
+console.log("Request URL:", `${API_URL}/monday/${boardId}`);
 
     console.log("HTTP Status:", response.status);
 
